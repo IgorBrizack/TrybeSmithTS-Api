e7de564770f6afa993e2fe6829ca6707f06aabab
Igor Brizack <igor_brizack@hotmail.com>
2022-10-07 08:11:16 -0300

    requisito 1 parcialmente concluido

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import ProductRouter from './routes/product.routes';
+
+// const router = Router();
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/products', ProductRouter);
 
 export default app;
